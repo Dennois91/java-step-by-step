f04da9c38f348e3bdbb7786a1eb09a7e23e7c778
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-07-18 11:04:31 +0200

    First Commit

---
+++ ovning2.7/Ovning.java
@@ -0,0 +1,8 @@
+class Ovning {
+    public static void main(String[] args) {
+        String a = "Programspråk" , b;
+        b = a.substring(4, 8) + a.charAt(5);
+        System.out.println(b);
+    }
+    
+}
---
+++ ovning2.8/Ovning.java
@@ -0,0 +1,12 @@
+class Ovning {
+    public static void main(String[] args) {
+        var a = " Erik Andersson 860314-2714";
+        a = a.trim();
+        var i = a.lastIndexOf(' ') + 3;
+        var j = a.lastIndexOf(' ') + 5;
+        var i1 = a.lastIndexOf(' ') + 5;
+        var j1 = a.lastIndexOf(' ') + 7;
+        var b = a.substring(i1, j1) + "/" + a.substring(i, j);
+        System.out.println(b);
+    }
+}
