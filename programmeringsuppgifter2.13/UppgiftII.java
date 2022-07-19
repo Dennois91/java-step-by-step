0c0e2a8d70f35cd46379bb35fe93f039ff9c0127
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-07-19 12:59:16 +0200

    Kappitell 2 klart

--- programmeringsuppgifter2.13/UppgiftII.java
+++ programmeringsuppgifter2.13/UppgiftII.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 class UppgiftII {
     public static void main(String[] args) {
-        
-        
+
+        String namn, efternamn;
+        try (var scan = new Scanner(System.in)) {
+            System.out.println("Skriv in ditt Efternamn mellanslag förnamn");
+            namn = scan.nextLine();
+        }
+
+        namn = namn.trim();                 //Remove spaces before and after string
+        var i = namn.lastIndexOf(' ');  //Give int i value of last space in string
+        var j = 0;                          //Give int j value of 0
+        efternamn = namn.substring(j, i);   //Give value of string namn between index j-i
+        efternamn = efternamn.toUpperCase();//Make string uppercase
+        System.out.println(efternamn);      //Print String
     }
-    
 }
---
+++ programmeringsuppgifter2.13/UppgiftIII.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class UppgiftIII {
+    public static void main(String[] args) {
+
+        String name, surname;
+        try (var scan = new Scanner(System.in)) {
+            System.out.println("Type you're first name:");
+            name = scan.next();
+            System.out.println("Type you're last name:");
+            surname = scan.next();
+        }
+
+        surname = surname.toUpperCase();
+        
+        System.out.println("Hello " + surname);
+
+    }
+    
+}
